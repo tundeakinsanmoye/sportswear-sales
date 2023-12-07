16dbaeee0624cad994d412d5efe44502d208c7e8
Tunde Akinsanmoye <tundeakinsanmoye@gmail.com>
2023-12-07 18:00:46 +0100

    I commit to the changes in this file

---
+++ index2.js
@@ -0,0 +1,14 @@
+let countEl = document.getElementById("count-el")
+
+let soldParagraph = document.getElementById("sold")
+
+
+let count = 0
+
+function item() {
+    count += 1
+    countEl.textContent = count
+}
+function purchase() {
+    soldParagraph.textContent = "Payment received! Thank you"
+}
